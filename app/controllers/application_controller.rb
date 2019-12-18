83cbb8b177b0fa82beb5e3a21d17b96b7b6fadb6
cdallasanta <chris.dallasanta@gmail.com>
2019-12-17 23:07:38 -0800

    removing controller test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < ActionController::API
   end
 
   def fallback_index_html
-    render json: Site.find(1)
-    # render :file => 'public/index.html'
+    render :file => 'public/index.html'
   end
 end
