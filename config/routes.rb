7508f93674de75b827e8dc4049dba7713b7a6f3c
cdallasanta <chris.dallasanta@gmail.com>
2019-12-17 23:21:38 -0800

    added static controller to avoid bloat

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#is_logged_in?'
-  
 
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+
+  get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
 end
