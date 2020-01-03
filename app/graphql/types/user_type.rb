3f2dc7597b9edec74433efff54aa9bd356d16fd6
cdallasanta <chris.dallasanta@gmail.com>
2020-01-02 16:13:57 -0800

    Source updated at 2020-01-02 16:13:57 -0800.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :fullname, String, null: false
+    # TODO: remove bottom line for security
     field :email, String, null: false
   end
 end
