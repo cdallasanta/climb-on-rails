fc3dd478a1e849ebacf11a09e817ffaffe901193
cdallasanta <chris.dallasanta@gmail.com>
2020-01-07 17:28:09 -0800

    Source updated at 2020-01-07 17:28:09 -0800.

--- client/src/containers/elementList.js
+++ client/src/containers/elementList.js
@@ -1,30 +1,32 @@
 import React, {Component} from 'react';
 import "../stylesheets/elements.scss";
 import ElementCard from '../components/elementCard';
-import {graphql} from 'react-apollo';
+import {graphql, Query} from 'react-apollo';
 import {getElementsQuery} from '../queries/queries';
 import * as compose from 'lodash.flowright';
 
 class ElementList extends Component {
-
-  renderElements= () => {
-    const data = this.props.getElementsQuery
-    if (!data.loading){
-      return data.elements.map(elem => {
-        return <ElementCard data={elem} key={elem.id} location={this.props.location} />;
-      });
-    }
+  renderElements= (data) => {
+    return data.elements.map(elem => {
+      return <ElementCard data={elem} key={elem.id} location={this.props.location} />;
+    });
   }
 
   render() {
     return (
-      <div className="elements" id="elements-list">
-        {this.renderElements()}
-      </div>
+      <Query
+        query={getElementsQuery}>
+        {({loading, data}) => {
+          if (loading) return null;
+          
+          return <div className="elements" id="elements-list">
+            {this.renderElements(data)}
+          </div>
+
+        }}
+      </Query>
     )
   }
 }
 
-export default compose(
-  graphql(getElementsQuery, {name:"getElementsQuery"})
-)(ElementList);
+export default ElementList;
