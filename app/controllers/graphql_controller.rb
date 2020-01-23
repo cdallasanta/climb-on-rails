43350be42c077beec3c87d3835bdbc158b2cbc33
cdallasanta <chris.dallasanta@gmail.com>
2020-01-23 14:11:55 -0800

    tried moving graphql tests into it's own, won't work for signin

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -12,7 +12,7 @@ class GraphqlController < ApplicationController
       session: session,
       current_user: current_user
     }
-    result = ClimbOnReactSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = ClimbOnSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue => e
     raise e unless Rails.env.development?
--- app/graphql/climb_on_schema.rb
+++ app/graphql/climb_on_schema.rb
@@ -1,4 +1,4 @@
-class ClimbOnReactSchema < GraphQL::Schema
+class ClimbOnSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
