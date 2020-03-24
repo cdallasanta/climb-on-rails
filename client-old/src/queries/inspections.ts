6cdfe8e8f7da3c9c5042b32e6151092db28f1832
cdallasanta <chris.dallasanta@gmail.com>
2020-03-24 16:06:16 -0700

    moved folders into src where they should be

---
+++ client-old/src/queries/inspections.ts
@@ -0,0 +1,184 @@
+import { gql } from 'apollo-boost';
+
+const getPreuseInspectionQuery: string = gql`
+query($elementId: Int!, $date: String!) {
+  element(id:$elementId) {
+    id
+    setupElementInstructions
+    setupEquipmentInstructions
+    setupEnvironmentInstructions
+    takedownElementInstructions
+    takedownEquipmentInstructions
+    takedownEnvironmentInstructions
+    preuseInspection(date:$date) {
+      id
+      setupAttributes: setup{
+        id
+        users {
+          fullname
+        }
+        isComplete
+        sectionsAttributes: sections{
+          id
+          title
+          complete
+          commentsAttributes: comments {
+            id
+            content
+            user {
+              fullname
+            }
+          }
+        }
+      }
+      takedownAttributes: takedown{
+        id
+        users {
+          fullname
+        }
+        climbsAttributes: climbs {
+          id
+          block1
+          block2
+          block3
+          block4
+          rope {
+            id
+            identifier
+          }
+        }
+        sectionsAttributes: sections{
+          id
+          title
+          complete
+          commentsAttributes: comments {
+            id
+            content
+            user {
+              fullname
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const getPeriodicInspectionQuery: string = gql`
+query($elementId: Int!, $date: String!) {
+  element(id:$elementId) {
+    id
+    periodicElementInstructions
+    periodicEquipmentInstructions
+    periodicEnvironmentInstructions
+    periodicInspection(date:$date){
+      id
+      users {
+        fullname
+      }
+      sectionsAttributes: sections{
+        id
+        title
+        complete
+        commentsAttributes: comments {
+          id
+          content
+          user {
+            fullname
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const savePeriodicMutation: string = gql`
+mutation($data: PeriodicInput!) {
+  savePeriodic(data: $data){
+    status
+    errors
+    periodicInspection{
+      id
+      users {
+        fullname
+      }
+      sectionsAttributes: sections{
+        id
+        title
+        complete
+        commentsAttributes: comments {
+          id
+          content
+          user {
+            fullname
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const savePreuseMutation: string = gql`
+mutation($data: PreuseInput!) {
+  savePreuse(data: $data){
+    status
+    errors
+    preuseInspection {
+      id
+      setupAttributes: setup{
+        id
+        users {
+          fullname
+        }
+        isComplete
+        sectionsAttributes: sections{
+          id
+          title
+          complete
+          commentsAttributes: comments {
+            id
+            content
+            user {
+              fullname
+            }
+          }
+        }
+      }
+      takedownAttributes: takedown{
+        id
+        users {
+          fullname
+        }
+        climbsAttributes: climbs {
+          id
+          block1
+          block2
+          block3
+          block4
+          rope {
+            id
+            identifier
+          }
+        }
+        sectionsAttributes: sections{
+          id
+          title
+          complete
+          commentsAttributes: comments {
+            id
+            content
+            user {
+              fullname
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+export { getPreuseInspectionQuery, getPeriodicInspectionQuery, savePeriodicMutation, savePreuseMutation }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './stylesheets/global.scss'
+import Alert from './components/alert'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Alert type="alert-success" messages={["test1", "test2"]}/>
     </div>
   );
 }
