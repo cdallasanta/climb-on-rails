894d04289be96f0e1d35eb4764f9ecacb523504d
cdallasanta <chris.dallasanta@gmail.com>
2020-03-25 14:11:32 -0700

    takedown migrated

--- client/src/components/inspections/takedown.tsx
+++ client/src/components/inspections/takedown.tsx
@@ -1,24 +1,45 @@
 import React, {Component} from 'react';
 import Section from '../../components/inspections/section';
 
-class Takedown extends Component {
+interface Props{
+  instructions: any;
+  data: any;
+  handleChange: (e: any) => void;
+  newComments: {
+    Element: {content: string},
+    Equipment: {content: string},
+    Environment: {content: string}
+  };
+  renderUpdatedBy: () => void;
+} // TODO: fix the typing of handleChange
+
+interface Climb {
+  id: number;
+  rope: {identifier: string};
+  block1: number;
+  block2: number;
+  block3: number;
+  block4: number;
+}
+
+class Takedown extends Component<Props> {
   renderSections = () => {
     return <>
       <Section {...this.props}
         instructions={this.props.instructions.elementInstructions}
-        data={this.props.data.sectionsAttributes.find(s => s.title === "Element")}
+        data={this.props.data.sectionsAttributes.find((s: {title: string}) => s.title === "Element")}
         handleChange={this.props.handleChange}
         newComment={this.props.newComments.Element.content}
         inspection="takedown" />
       <Section {...this.props}
         instructions={this.props.instructions.equipmentInstructions}
-        data={this.props.data.sectionsAttributes.find(s => s.title === "Equipment")}
+        data={this.props.data.sectionsAttributes.find((s: {title: string}) => s.title === "Equipment")}
         handleChange={this.props.handleChange}
         newComment={this.props.newComments.Equipment.content}
         inspection="takedown" />
       <Section {...this.props}
         instructions={this.props.instructions.environmentInstructions}
-        data={this.props.data.sectionsAttributes.find(s => s.title === "Environment")}
+        data={this.props.data.sectionsAttributes.find((s: {title: string}) => s.title === "Environment")}
         handleChange={this.props.handleChange}
         newComment={this.props.newComments.Environment.content}
         inspection="takedown" />
@@ -36,7 +57,7 @@ class Takedown extends Component {
           <th>Block 4</th>
         </tr></thead>
         <tbody>
-          {this.props.data.climbsAttributes.map((climb, i) =>{
+          {this.props.data.climbsAttributes.map((climb: Climb, i: number) =>{
           return <tr key ={i}>
             <td>{climb.rope.identifier}</td>
             <td>
