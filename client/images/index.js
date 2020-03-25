1a9ad14da24fc00f8d07d3c725adc5f99d2e8df9
Chris Dalla Santa <chris.dallasanta@gmail.com>
2020-03-25 13:55:15 -0700

    hidden changes from ide? files removed?

--- client/images/index.js
+++
@@ -1,5 +0,0 @@
-import periodicForm from './AlutianMaidenhairFern.JPG';
-import preuseForm from './HappyCreek.JPG';
-import userPage from './ThunderCreek.JPG';
-
-export {periodicForm, preuseForm, userPage}
