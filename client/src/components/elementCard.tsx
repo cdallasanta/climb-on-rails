9a5a5f68a9ea1f5f0cd8c3c5ce3d3c098fceebaf
cdallasanta <chris.dallasanta@gmail.com>
2020-03-25 13:48:49 -0700

    element card migrated

---
+++ client/src/components/elementCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link, withRouter, RouteComponentProps} from 'react-router-dom';
+
+interface Props extends RouteComponentProps {
+  data: {
+    name: string,
+    id: number
+  };
+}
+
+const ElementCard = ({data: {name, id}, location,}: Props) => {
+  return (
+    <Link to={location.pathname + `/elements/${id}/new`} style={{textDecoration: 'none'}}>
+      <div className="element-card">
+        <h1>{name}</h1>
+        
+        <img src={require(`../images/${name.replace(/[^0-9a-z]/gi, "")}.png`)} alt={`${name}`} />
+      </div>
+    </Link>
+  )
+}
+
+export default withRouter(ElementCard);
