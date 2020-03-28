400a136e5a9ef2601639ea801edf2c08c82dd961
cdallasanta <chris.dallasanta@gmail.com>
2020-03-28 13:15:57 -0700

    trying master again...]

--- client/src/containers/sessions/login.tsx
+++ client/src/containers/sessions/login.tsx
@@ -95,7 +95,7 @@ class Login extends Component<Props, State> {
 
         <div id="login-form-div">
           <h1>Welcome to<br />
-          Climb On!v2</h1>
+          Climb On! - test</h1>
 
           <form id="login-form" onSubmit={this.handleSubmit}>
             <div>
