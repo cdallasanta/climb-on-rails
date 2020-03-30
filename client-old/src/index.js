0f39df74b32934e159ccc4718dd149d76f3689e5
cdallasanta <chris.dallasanta@gmail.com>
2020-03-30 14:38:03 -0700

    left the development uri on index

--- client-old/src/index.js
+++ client-old/src/index.js
@@ -9,7 +9,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const link = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
   credentials: 'same-origin'
 });
 
